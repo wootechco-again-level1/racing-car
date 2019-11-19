c82ebfedf304f0ae14cf6cc51c5c7bc1950594ef
pkch93 <star_final@naver.com>
2019-11-19 11:27:17 +0900

    refactor: 매직넘버 상수화

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,19 +1,38 @@
 package racingcar.domain;
 
 public class Car {
+    private static final int MIN_MOVABLE_NUMBER = 4;
+    private static final int MOVE = 1;
+    private static final int STOP = 0;
+
     private final String name;
-    private int distance = 0;
+    private int distance;
 
     public Car(String name) {
+        this(name, 0);
+    }
+
+    public Car(String name, int distance) {
         CarValidator.validCarLength(name);
+
         this.name = name;
+        this.distance = distance;
     }
 
     public int move(int movable) {
-        if (movable >= 4) {
-            distance += 1;
-        }
+        distance += checkMovable(movable);
+        return distance;
+    }
 
+    private int checkMovable(int movable) {
+        return movable >= MIN_MOVABLE_NUMBER ? MOVE : STOP;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDistance() {
         return distance;
     }
 }
