a14796f220615ed61ac583417d8d03da1f837f75
이두호 <ddu0422@naver.com>
2019-11-24 00:49:53 +0900

    feat: 자동차들 이동 구현

--- src/main/java/domain/Cars.java
+++ src/main/java/domain/Cars.java
@@ -1,4 +1,27 @@
 package domain;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Cars {
+    private final List<Car> list;
+
+    public Cars(final List<Car> cars) {
+        this.list = cars;
+    }
+
+    public Cars move(final MoveStrategy moveStrategy) {
+        return new Cars(list.stream()
+                .map(car -> car.move(moveStrategy))
+                .collect(Collectors.toList()))
+                ;
+    }
+
+    public int getSize() {
+        return list.size();
+    }
+
+    public Car get(final int index) {
+        return list.get(index);
+    }
 }
