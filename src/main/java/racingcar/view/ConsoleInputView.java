c8dbc4da2bfd95c5c3ed7272bc25ed53767b6ee3
pkch93 <star_final@naver.com>
2019-11-25 22:42:13 +0900

    refactor: InputValidator
    - validName, validNumberOfRound가 부적절한 경우를 찾으므로 invalid*로 변경
    - invalidName에서 부적절한 입력의 경우 오류 메세지 출력
    - ConsoleInputView에서 이름 입력 메세지 한번만 출력되도록 변경

--- src/main/java/racingcar/view/ConsoleInputView.java
+++ src/main/java/racingcar/view/ConsoleInputView.java
@@ -10,10 +10,10 @@ public class ConsoleInputView implements InputView {
     @Override
     public String inputCarNames() {
         String names;
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         do {
-            System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
             names = scanner.nextLine();
-        } while (InputValidator.validName(names));
+        } while (InputValidator.invalidName(names));
 
         return names;
     }
@@ -24,7 +24,7 @@ public class ConsoleInputView implements InputView {
         do {
             System.out.println("시도할 횟수는 몇회인가요?");
             input = scanner.nextLine();
-        } while (InputValidator.validNumberOfRound(input));
+        } while (InputValidator.invalidNumberOfRound(input));
 
         return Integer.parseInt(input);
     }
